2e807772c4b4e36437e2b87087e6870a743e9b63
Greg Orvik <gvorvik@gmail.com>
2018-07-17 11:50:21 -0500

    add delete token route

--- server/server.js
+++ server/server.js
@@ -144,6 +144,15 @@ app.post('/users', (req, res) => {
 
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token)
+    .then(() => {
+        res.sendStatus(200);
+    }, () => {
+        res.sendStatus(400);
+    });
+})
+
 app.listen(PORT, () => {
     console.log(`app listening at ${PORT}`);
 })
